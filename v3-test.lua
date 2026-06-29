8c802977a82283a74fc08503797d53ebfefa8a58
JualNasiRendang <radikaradikaff@gmail.com>
2026-06-29 15:09:18 +0700

    Update v3-test.lua

--- v3-test.lua
+++ v3-test.lua
@@ -1,9 +1,2 @@
+loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/m0gvhwcn"))()
 
-
-local func, err = loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/m0gvhwcn"))()
-
-if func then
-    func()  -- jalankan
-else
-    print("Error:", err)
-end
