a6943bed61fa9f90f93eda3df14972769092b66d
JualNasiRendang <radikaradikaff@gmail.com>
2026-07-06 14:58:10 +0700

    Create jnkiev2.lua

---
+++ jnkiev2.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e394e224e443900dd8d63e7a2ee0c90b42f8d3ea3e2f07bd9ed3fa1b74f82657/download"))()
