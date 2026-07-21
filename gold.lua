ae998c53251fa424e1be660cc2291d7dde8a6857
JualNasiRendang <radikaradikaff@gmail.com>
2026-07-22 03:40:20 +0700

    Update gold.lua

--- gold.lua
+++ gold.lua
@@ -1,3 +1,3 @@
 getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/9ce1adff1f218ebb2628aaceec09421d2a01237a5be8debf19519a039c202076/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/416a0d3ccc90a6a3c90591e1243caf8871195b9f6e1b3b6a26afd4b285683114/download"))()
 
